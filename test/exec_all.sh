24fff0bdfda23d4357b77d0bf70be3a80aebd001
Lucas Pena <lpena7@illinois.edu>
2017-12-03 17:29:52 -0600

    small changes, preparing for presentation

--- test/exec_all.sh
+++ test/exec_all.sh
@@ -3,5 +3,7 @@
 for file in `ls *.actor`;
   do
     printf "Running $file\n"
-    krun -d ../src $file
+    krun -d ../src $file > temp.xml
+    xmllint --format temp.xml | tail -n +2
+    rm temp.xml
 done;
